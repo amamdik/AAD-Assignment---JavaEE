241843ff02997e74475672590dbc616908b3d7e8
amamdik <amamdik@gmail.com>
2024-02-09 06:29:31 +0530

    content updated in CORSFilter

---
+++ AAD_Assignment_JavaEE/src/main/java/lk/ijse/gdse66/AAD_Assignment_JavaEE/filter/CORSFilter.java
@@ -0,0 +1,24 @@
+package lk.ijse.gdse66.AAD_Assignment_JavaEE.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CORSFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        System.out.println("CORS Filter");
+
+        String origin = req.getHeader("Origin");
+        if (origin.contains(getServletContext().getInitParameter("origin"))) {
+            res.setHeader("Access-Control-Allow-Origin", origin);
+            res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,HEADER");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+            res.setHeader("Access-Control-Expose-Headers", "Content-Type");
+        }
+        chain.doFilter(req, res);
+    }
+}
